8b3c2cc3f0cc880f69e620959cefbb10e6c4d52a
zmagnelia <115034390+zmagnelia@users.noreply.github.com>
2022-11-30 12:36:00 -0500

    Add files via upload

---
+++ app/pages/streamlit test2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 29 14:50:19 2022
+
+@author: Zoe Magnelia
+"""
+
+import streamlit as st
+import pandas as pd
+
+header = st.beta_container()
+description = st.beta_container()
+
+with header:
+    st.title('test streamlit app')
+    
