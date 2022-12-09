56019910219d7b9104f6efd57acb77ca221efe6c
mnagler1 <115102670+mnagler1@users.noreply.github.com>
2022-12-09 11:43:52 -0500

    Update 2_Credit_Score_Predictor.py

--- app/pages/2_Credit_Score_Predictor.py
+++ app/pages/2_Credit_Score_Predictor.py
@@ -11,11 +11,13 @@ import gradio as gr
 import math
 import os
 
+#Loading the data. If read_csv does not work with data/cleaner_cred_score_classifier, please import local data file location
 data = pd.read_csv("data/cleaner_cred_score_classifier.csv")
 #data = pd.read_csv(import local csv data)
 data = data.drop_duplicates(subset="Customer_ID", keep = 'first', inplace=False)
 
 st.session_state.update(st.session_state)
+#Main Class
 class creditScore(Page):
     def __init__(self, data, **kwargs):
         name = "Credit Score"
@@ -25,9 +27,6 @@ class creditScore(Page):
         self.kwargs = kwargs
         self.modelBuilt = False
         self.score = ['Poor','Standard','Good']
-        #print(creditScoreModel)
-        # self.creditScoreModel = creditScoreModel()
-        # self.model = self.creditScoreModel.loaded_model
 
     def content(self):
         
@@ -37,20 +36,11 @@ class creditScore(Page):
         """Returns the title of the page"""
         st.header(f"{self.name}")
 
+        #Pulling inputs from 1_fill_information file
     def input(self):
 
         st.header('Inputs')
         pt = pd.DataFrame({k:[v] for k,v in st.session_state.items()})
-        
-        # st.markdown(f"session state Occupation {st.session_state['Occupation']}")
-        # st.markdown('session state Age', st.session_state['Age'])
-        # st.markdown('session state Annual Income', st.session_state['Annual_Income'])
-        # st.markdown('session state Number of Bank Accounts', st.session_state['Num_Bank_Accounts'])
-        # st.markdown('session state Number of Credit Cards', st.session_state['Num_Credit_Card'])
-        # st.markdown('session state Number of Delayed Payments', st.session_state['Num_of_Delayed_Payment'])
-        # st.markdown('session state Number of Credit Inquiries', st.session_state['Num_Credit_Inquiries'])
-        # st.markdown('session state Outstanding Debt', st.session_state['Outstanding_Debt'])
-        # st.markdown('session state Credit History Age', st.session_state['Credit_History_Age'])
 
         self.occupation = pt['Occupation'][0]
         self.age = pt['Age'][0]
@@ -67,8 +57,6 @@ class creditScore(Page):
         pt = pt.transpose()
         st.table(pt)
 
-        print(self.inputValues)
-
         self.buttonPressed = st.button("Calculate Score")
         
     @st.cache(suppress_st_warning=True)
@@ -79,14 +67,14 @@ class creditScore(Page):
 
         # Initialize the structured data classifier.
         clf = autokeras.StructuredDataClassifier(
-            max_trials=1, # We will do 10 iterations of model improvement
+            max_trials=1,
             overwrite=True, # Overwrite prior training attempts
         ) 
 
         # Fit the model to the data that we used earlier
         clf.fit(
-            x = cleanData, # Measurements of cleanData2 dimensions
-            y = data['Credit_Score'], # fico score
+            x = cleanData,
+            y = data['Credit_Score'], # fico score good/standard/poor
             epochs=5 # The number of iterations to train each attempted model
         )
 
@@ -97,21 +85,15 @@ class creditScore(Page):
 
         
 
-
+        #Predicting the range of score
     def predict(self):
         self.loaded_model = tensorflow.keras.models.load_model("score_model_autokeras", custom_objects=autokeras.CUSTOM_OBJECTS)
-        #pullModel = creditScoreModel()
-        #self.loaded_model = tensorflow.keras.models.load_model(self.model, custom_objects=autokeras.CUSTOM_OBJECTS)
         # Make a prediction using the model. This returns a numeric integer output (e.g., 1)
         prediction = self.loaded_model.predict(self.inputValues)
-        #prediction = self.model.predict(self.inputValues)
-        print('prediction', prediction)
         # Return the answer
         maxIndex = np.where(prediction == np.amax(prediction))[1][0]
-        print(maxIndex)
         score_prediction = self.score[maxIndex]
         percent = round(prediction[0][maxIndex] * 100, 2)
-        print(percent)
         # Return the answer
         if score_prediction == 'Poor':
             self.scoreRange = "There is a " + str(percent) + "% chance that your score falls between 300 - 629"
