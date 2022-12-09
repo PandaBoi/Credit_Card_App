ed4f66f74659f081b19c6812c2dddef5a85e6904
mnagler1 <115102670+mnagler1@users.noreply.github.com>
2022-12-08 21:43:51 -0500

    Update 2_Credit_Score_Predictor.py

--- app/pages/2_Credit_Score_Predictor.py
+++ app/pages/2_Credit_Score_Predictor.py
@@ -3,16 +3,17 @@ from utils.page import Page
 import numpy as np
 import autokeras
 import tensorflow
-tensorflow.config.experimental_run_functions_eagerly(True)
-
 import tensorflow.keras.models
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+from models.creditScoreModel import creditScoreModel
 import pandas as pd
 import gradio as gr
 import math
+import os
 
 data = pd.read_csv("data/cleaner_cred_score_classifier.csv")
+#data = pd.read_csv(import local csv data)
 data = data.drop_duplicates(subset="Customer_ID", keep = 'first', inplace=False)
 
 st.session_state.update(st.session_state)
@@ -25,6 +26,9 @@ class creditScore(Page):
         self.kwargs = kwargs
         self.modelBuilt = False
         self.score = ['Poor','Standard','Good']
+        #print(creditScoreModel)
+        # self.creditScoreModel = creditScoreModel()
+        # self.model = self.creditScoreModel.loaded_model
 
     def content(self):
         
@@ -35,9 +39,19 @@ class creditScore(Page):
         st.header(f"{self.name}")
 
     def input(self):
-        
+
         st.header('Inputs')
         pt = pd.DataFrame({k:[v] for k,v in st.session_state.items()})
+        
+        # st.markdown(f"session state Occupation {st.session_state['Occupation']}")
+        # st.markdown('session state Age', st.session_state['Age'])
+        # st.markdown('session state Annual Income', st.session_state['Annual_Income'])
+        # st.markdown('session state Number of Bank Accounts', st.session_state['Num_Bank_Accounts'])
+        # st.markdown('session state Number of Credit Cards', st.session_state['Num_Credit_Card'])
+        # st.markdown('session state Number of Delayed Payments', st.session_state['Num_of_Delayed_Payment'])
+        # st.markdown('session state Number of Credit Inquiries', st.session_state['Num_Credit_Inquiries'])
+        # st.markdown('session state Outstanding Debt', st.session_state['Outstanding_Debt'])
+        # st.markdown('session state Credit History Age', st.session_state['Credit_History_Age'])
 
         self.occupation = pt['Occupation'][0]
         self.age = pt['Age'][0]
@@ -50,15 +64,18 @@ class creditScore(Page):
         self.history = pt["Credit_History_Age"][0]
         self.inputValues = [self.occupation, self.age, self.annualIncome, self.numBankAcc, self.numCreditCards,
                             self.numDelPay, self.inquiries, self.debt, self.history]
+
         pt = pt.transpose()
         st.table(pt)
-        
+
+        print(self.inputValues)
+
         self.buttonPressed = st.button("Calculate Score")
         
     @st.cache(suppress_st_warning=True)
     def buildModel(self):
         self.modelBuilt = True
-        cleanData = self.data.drop(labels = ["Interest_Rate","Num_of_Loan","Delay_from_due_date", "Changed_Credit_Limit","Credit_Utilization_Ratio","Total_EMI_per_month","Amount_invested_monthly","Monthly_Balance","debtconsolidation","payday","notspecified","nodata","mortgage","credit-builder","auto","personal","homeequity","Credit_Score", "student"], axis = 1)
+        cleanData = self.data.drop(labels = ["Customer_ID","Interest_Rate","Num_of_Loan","Delay_from_due_date", "Changed_Credit_Limit","Credit_Utilization_Ratio","Total_EMI_per_month","Amount_invested_monthly","Monthly_Balance","debtconsolidation","payday","notspecified","nodata","mortgage","credit-builder","auto","personal","homeequity","Credit_Score", "student"], axis = 1)
         #Defines what the outputs may be
 
         # Initialize the structured data classifier.
@@ -79,11 +96,16 @@ class creditScore(Page):
         scoreModel = clf.export_model()
         scoreModel.save("score_model_autokeras", save_format = 'tf')
 
+        
+
 
     def predict(self):
-        self.loaded_model = tensorflow.keras.models.load_model("models/score_model_autokeras", custom_objects=autokeras.CUSTOM_OBJECTS)
+        self.loaded_model = tensorflow.keras.models.load_model("score_model_autokeras", custom_objects=autokeras.CUSTOM_OBJECTS)
+        #pullModel = creditScoreModel()
+        #self.loaded_model = tensorflow.keras.models.load_model(self.model, custom_objects=autokeras.CUSTOM_OBJECTS)
         # Make a prediction using the model. This returns a numeric integer output (e.g., 1)
         prediction = self.loaded_model.predict(self.inputValues)
+        #prediction = self.model.predict(self.inputValues)
         print('prediction', prediction)
         # Return the answer
         maxIndex = np.where(prediction == np.amax(prediction))[1][0]
@@ -106,6 +128,6 @@ page = creditScore(data, test = 'testing')
 page.title()
 page.content()
 page.input()
-# page.buildModel() # can use pre-trained model after first use
+page.buildModel()
 if page.buttonPressed:
     page.predict()
